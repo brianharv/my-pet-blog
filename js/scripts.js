f25b39175f625ee8e56e8e44325ed38d5b0198d8
Brian Harvey <brian.harv3y@gmail.com>
2020-08-11 11:58:57 -0700

    add this argument
    Co-authored-ny: David McCaslin <davidmccaslin94@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
   $("#change-mode").click(function() {
     $("body").toggleClass("dark-mode");
   });
+
+  $("p").click(function() {
+    $(this).toggleClass("paragraph-border");
+  });
 });
+
